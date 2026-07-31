5a154f01f9e40e89e157b931e9d430154f05a227
Akash <smurtiakashjena@gmail.com>
2026-07-31 21:27:46 +0530

    added ai powered ecommerce

---
+++ SpringEcomAI/src/main/java/com/akashh/SpringEcom/controller/ChatBotController.java
@@ -0,0 +1,22 @@
+package com.akashh.SpringEcom.controller;
+
+import com.akashh.SpringEcom.service.ChatBotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/chat")
+@CrossOrigin
+public class ChatBotController {
+
+    @Autowired
+    private ChatBotService chatBotService;
+
+    @GetMapping("/ask")
+    public ResponseEntity<String> askBot(@RequestParam String message){
+
+        String response = chatBotService.getBotResponse(message);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ SpringEcomAI/src/main/java/com/akashh/SpringEcom/model/dto/OrderItemRequest.java
@@ -0,0 +1,6 @@
+package com.akashh.SpringEcom.model.dto;
+
+public record OrderItemRequest(
+        int productId,
+        int quantity
+) {}
